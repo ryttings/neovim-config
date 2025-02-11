1e2faa89d46d4d26d85774db3257db55716e101e
scotrytt <scott.rytting@keysight.com>
2025-02-11 16:42:34 -0700

    Nushell default for windows & ignoring out dirs

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,12 +8,19 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = {
+            "\\.git\\",
+            "\\out\\"
+          }
+        },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
           },
         },
       })
+
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>o", builtin.find_files, {})
       vim.keymap.set("n", "<leader>f", builtin.live_grep, {})
