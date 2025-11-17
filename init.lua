97683ed4a0e781544969aa140e0561ba09ac8870
Scott Rytting <scott.rytting@keysight.com>
2025-11-17 14:55:45 -0700

    Shell change from env

--- init.lua
+++ init.lua
@@ -39,7 +39,7 @@ vim.keymap.set('v', '<leader>cu', ':s/\\l/\\U&/g', { noremap = true }, { desc = 
 
 vim.cmd('autocmd TermOpen * setlocal nonumber norelativenumber')
 vim.opt.termguicolors = false
-vim.opt.shell = 'nu.exe'
+vim.opt.shell = os.getenv("SHELL")
 
 local python_cmd = os.getenv("PYTHON") or "python3"
 local usefulthings = os.getenv("USEFULTHINGS") or "~/usefulthings"
@@ -48,6 +48,8 @@ local cmd_shell = os.getenv("CMD_SHELL") or vim.opt.shell
 local llm_py = "llm.py"
 
 local llm_script = string.format("%s/tools/%s", usefulthings, llm_py)
+print(llm_script)
+
 vim.keymap.set('c', 'llm',
     function()
         vim.cmd('new')
